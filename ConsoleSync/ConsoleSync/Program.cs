404aa156d142c564204271294468e5f87f3e7dc1
Jose Luis Carrillo Aguilar <jose.carrillo@microsoft.com>
2020-05-31 17:57:54 +0200

    Run wf call and repos id call added

--- ConsoleSync/ConsoleSync/Program.cs
+++ ConsoleSync/ConsoleSync/Program.cs
@@ -8,8 +8,7 @@ namespace ConsoleSync
     {
         static void Main(string[] args)
         {
-            var secretValue = System.Text.Encoding.UTF8.GetBytes(args[1]);
-
+            // Get public key
             var client = new RestClient("https://api.github.com/orgs/azure-community-day/actions/secrets/public-key");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
@@ -19,17 +18,39 @@ namespace ConsoleSync
             string public_key = Convert.ToString(api.key);
             string key_id = Convert.ToString(api.key_id);
 
+            var secretValue = System.Text.Encoding.UTF8.GetBytes(args[1]);
             var publicKey = Convert.FromBase64String(public_key);
             var sealedPublicKeyBox = Sodium.SealedPublicKeyBox.Create(secretValue, publicKey);
-
             //Console.WriteLine(Convert.ToBase64String(sealedPublicKeyBox));
 
+            // Get repositories ids used in AZURE_WEBAPP_PUBLISH_PROFILE
+            client = new RestClient("https://api.github.com/orgs/azure-community-day/actions/secrets/AZURE_WEBAPP_PUBLISH_PROFILE/repositories");
+            client.Timeout = -1;
+            request = new RestRequest(Method.GET);
+            request.AddHeader("Authorization", "token " + args[0]);
+            response = client.Execute(request);
+            dynamic responseParsed = JObject.Parse(response.Content);
+            string repo1_id = Convert.ToString(responseParsed.repositories[0].id);
+            string repo2_id = Convert.ToString(responseParsed.repositories[1].id);
+            //string public_key = Convert.ToString(api.key);
+            //Console.WriteLine(response.Content);
+
+            // Update secret org AZURE_WEBAPP_PUBLISH_PROFILE
             client = new RestClient("https://api.github.com/orgs/azure-community-day/actions/secrets/AZURE_WEBAPP_PUBLISH_PROFILE");
             client.Timeout = -1;
             request = new RestRequest(Method.PUT);
             request.AddHeader("Authorization", "token " + args[0]);
             request.AddHeader("Content-Type", "application/json");
-            request.AddParameter("application/json", "{\n  \"encrypted_value\": \""+Convert.ToBase64String(sealedPublicKeyBox)+"\",\n  \"key_id\": \""+key_id+"\",\n  \"visibility\": \"selected\",\n  \"selected_repository_ids\": [268068809,268047790]\n}", ParameterType.RequestBody);
+            request.AddParameter("application/json", "{\n  \"encrypted_value\": \""+Convert.ToBase64String(sealedPublicKeyBox)+"\",\n  \"key_id\": \""+key_id+"\",\n  \"visibility\": \"selected\",\n  \"selected_repository_ids\": ["+repo1_id+","+repo2_id+"]\n}", ParameterType.RequestBody);
+            response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+
+            //Run vacd.api workflow, action id 119965941
+            client = new RestClient("https://api.github.com/repos/azure-community-day/vacd.api/actions/runs/119965941/rerun");
+            client.Timeout = -1;
+            request = new RestRequest(Method.POST);
+            request.AddHeader("Authorization", "token " + args[0]);
+            request.AddParameter("text/plain", "", ParameterType.RequestBody);
             response = client.Execute(request);
             //Console.WriteLine(response.Content);
         }
