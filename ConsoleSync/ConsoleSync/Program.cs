d36af378ed8d24b5aea3771a40afb170459287bf
Jose Luis Carrillo Aguilar <jose.carrillo@microsoft.com>
2020-06-02 18:08:07 +0200

    Rollback to 264b548

--- ConsoleSync/ConsoleSync/Program.cs
+++ ConsoleSync/ConsoleSync/Program.cs
@@ -18,16 +18,6 @@ namespace ConsoleSync
             string public_key = Convert.ToString(api.key);
             string key_id = Convert.ToString(api.key_id);
 
-            //Value for AZURE_WEBAPP_PUBLISH_PROFILE
-            var secretValue = System.Text.Encoding.UTF8.GetBytes(args[1]);
-            var publicKey = Convert.FromBase64String(public_key);
-            var sealedPublicKeyBox = Sodium.SealedPublicKeyBox.Create(secretValue, publicKey);
-            //Console.WriteLine(Convert.ToBase64String(sealedPublicKeyBox));
-
-            //Value for AZURE_WEBAPP_NAME
-            var secretValue2 = System.Text.Encoding.UTF8.GetBytes(args[2]);
-            var sealedPublicKeyBox2 = Sodium.SealedPublicKeyBox.Create(secretValue, publicKey);
-
             // Get repositories ids used in AZURE_WEBAPP_PUBLISH_PROFILE
             client = new RestClient("https://api.github.com/orgs/azure-community-day/actions/secrets/AZURE_WEBAPP_PUBLISH_PROFILE/repositories");
             client.Timeout = -1;
@@ -41,6 +31,11 @@ namespace ConsoleSync
             //Console.WriteLine(response.Content);
 
             // Update secret org AZURE_WEBAPP_PUBLISH_PROFILE
+            var secretValue = System.Text.Encoding.UTF8.GetBytes(args[1]);
+            var publicKey = Convert.FromBase64String(public_key);
+            var sealedPublicKeyBox = Sodium.SealedPublicKeyBox.Create(secretValue, publicKey);
+            //Console.WriteLine(Convert.ToBase64String(sealedPublicKeyBox));
+
             client = new RestClient("https://api.github.com/orgs/azure-community-day/actions/secrets/AZURE_WEBAPP_PUBLISH_PROFILE");
             client.Timeout = -1;
             request = new RestRequest(Method.PUT);
@@ -50,17 +45,23 @@ namespace ConsoleSync
             response = client.Execute(request);
             //Console.WriteLine(response.Content);
 
-            // Update secret org AZURE_WEBAPP_NAME
+            // Get webapp name and change secret
+            string webappName = args[1].Split('$')[0].Split('=')[1];
+            Console.WriteLine("WebAppName is: " + webappName);
+
+            secretValue = System.Text.Encoding.UTF8.GetBytes(webappName);
+            sealedPublicKeyBox = Sodium.SealedPublicKeyBox.Create(secretValue, publicKey);
+
             client = new RestClient("https://api.github.com/orgs/azure-community-day/actions/secrets/AZURE_WEBAPP_NAME");
             client.Timeout = -1;
             request = new RestRequest(Method.PUT);
             request.AddHeader("Authorization", "token " + args[0]);
             request.AddHeader("Content-Type", "application/json");
-            request.AddParameter("application/json", "{\n  \"encrypted_value\": \"" + Convert.ToBase64String(sealedPublicKeyBox2) + "\",\n  \"key_id\": \"" + key_id + "\",\n  \"visibility\": \"selected\",\n  \"selected_repository_ids\": [" + repo1_id + "," + repo2_id + "]\n}", ParameterType.RequestBody);
+            request.AddParameter("application/json", "{\n  \"encrypted_value\": \"" + Convert.ToBase64String(sealedPublicKeyBox) + "\",\n  \"key_id\": \"" + key_id + "\",\n  \"visibility\": \"selected\",\n  \"selected_repository_ids\": [" + repo1_id + "]\n}", ParameterType.RequestBody);
             response = client.Execute(request);
             //Console.WriteLine(response.Content);
 
-            //Run vacd.api workflow, action id 119965941
+            //Run vacd.api workflow, action id 121672254
             client = new RestClient("https://api.github.com/repos/azure-community-day/vacd.api/actions/runs/121672254/rerun");
             client.Timeout = -1;
             request = new RestRequest(Method.POST);
