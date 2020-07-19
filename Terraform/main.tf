5c0b972a5a771f11299232b20b05cd18b31da971
Jose Luis Carrillo Aguilar <joselcaguilar@users.noreply.github.com>
2020-07-19 10:08:46 +0200

    Updated init

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,10 +5,6 @@ terraform {
     storage_account_name = "backendtfvacd"
     container_name       = "tfstate"
     key                  = "PlatformVACD.tfstate"
-    client_id                  = var.client_id
-    client_secret              = var.client_secret
-    tenant_id                  = var.tenant_id
-    subscription_id            = var.subscription_id
   }
 }
 
