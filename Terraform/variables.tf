b7370bd44d6a120282e0ea306ba19b6acf3ebf3f
Jose Luis Carrillo Aguilar <joselcaguilar@users.noreply.github.com>
2020-07-19 10:56:00 +0200

    Added cred vars again

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,9 +1,26 @@
 ###################
-# App Service Plan
+# General
 ###################
 variable "rg_name" {
   description = "Name of the Resource Group"
 }
+variable "client_id" {
+  description = "Application Id of the Service Principal that will deploy the IaC"
+  default     = ""
+}
+variable "client_secret" {
+  description = "Secret of the Service Principal that will deploy the IaC"
+  default     = ""
+}
+variable "tenant_id" {
+  description = "Tenant Id of the subscription"
+  default     = ""
+}
+variable "subscription_id" {
+  description = "Subscription Id where the IaC will be deployed"
+  default     = ""
+}
+
 ###################
 # App Service Plan
 ###################
